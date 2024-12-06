837a0b8f855925a699cd796e66028e3ff1d705e2
Brandon <itzbrandonlee@yahoo.com>
2024-12-05 16:41:10 -0800

    vehicle search query

--- includes/classes/vehicle.php
+++ includes/classes/vehicle.php
@@ -48,5 +48,55 @@ class Vehicle{
             die($e->getMessage());
         }
     }
+
+    function search_vehicle($branch, $start_date, $end_date){
+        try {
+            if(!empty($branch) && !empty($start_date) && !empty($end_date)){
+                $search_vehicles = $this->db->prepare("SELECT ")
+            } else if(!empty($branch)){
+                $search_vehicles = $this->db->prepare("SELECT 
+    v.car_id, 
+    v.c_name, 
+    v.description, 
+    v.model_year, 
+    v.manufacturer, 
+    v.color, 
+    vd.registration_num, 
+    vd.seat_capacity, 
+    vd.mileage, 
+    vd.rate, 
+    ft.fuel_type_name, 
+    ct.car_type_name, 
+    br.location
+FROM 
+    vehicle AS v
+JOIN 
+    vehicle_details AS vd ON v.car_id = vd.car_id
+JOIN 
+    fuel_type AS ft ON vd.fuel_type_id = ft.fuel_type_id
+JOIN 
+    car_type AS ct ON vd.type_car_id = ct.type_car_id
+JOIN 
+    branch AS br ON br.branch_id = vd.branch_id
+LEFT JOIN 
+    booking AS b ON vd.registration_num = b.vehicle_registration
+    AND NOT (
+        b.drop_date <  OR b.pickup_date > '2024-11-10'
+    )
+WHERE 
+    br.branch_id = 1
+    AND b.vehicle_registration IS NULL;);
+                $search_vehicles->execute();
+                if($search_vehicles->rowCount() > 0){
+                    return $search_vehicles->fetchAll(PDO::FETCH_OBJ);
+                }
+                else{
+                    return false;
+                }
+        } catch {
+            error_log($e->getMessage());
+            return null;
+        }
+    }
 }
 ?>
