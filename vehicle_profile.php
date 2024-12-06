4a9131a9380e2fc915a27c34492224122242e409
P4TOO <103611988+P4TOO@users.noreply.github.com>
2024-12-05 17:18:02 -0800

    Add files via upload

--- vehicle_profile.php
+++ vehicle_profile.php
@@ -1,41 +1,12 @@
 <?php
 require 'includes/init.php';
-// if(isset($_SESSION['user_id']) && isset($_SESSION['email'])){
-//     if(isset($_GET['id'])){
-//         $user_data = $user_obj->find_user_by_id($_GET['id']);
-//         if($user_data ===  false){
-//             header('Location: profile.php');
-//             exit;
-//         }
-//         else{
-//             if($user_data->id == $_SESSION['user_id']){
-//                 header('Location: profile.php');
-//                 exit;
-//             }
-//         }
-//     }
-// }
-// else{
-//     header('Location: logout.php');
-//     exit;
-// }
-
 $vehicle_data = $vehicle_obj->find_vehicle_by_id($_GET['id']);
 $all_branches = $branch_obj->get_all_branches();
-// if($vehicle_data === false){
-//     header('Location: profile.php');
-//     exit;
-// }
-// // CHECK FRIENDS
-// $is_already_friends = $frnd_obj->is_already_friends($_SESSION['user_id'], $user_data->id);
-// //  IF I AM THE REQUEST SENDER
-// $check_req_sender = $frnd_obj->am_i_the_req_sender($_SESSION['user_id'], $user_data->id);
-// // IF I AM THE REQUEST RECEIVER
-// $check_req_receiver = $frnd_obj->am_i_the_req_receiver($_SESSION['user_id'], $user_data->id);
-// // TOTAL REQUESTS
-// $get_req_num = $frnd_obj->request_notification($_SESSION['user_id'], false);
-// // TOTAL FRIENDS
-// $get_frnd_num = $frnd_obj->get_all_friends($_SESSION['user_id'], false);
+
+if(isset($_POST['branch']) && isset($_POST['departure-date']) && isset($_POST['return-date'])){
+    $result = $booking_obj->bookingSubmission($_GET['id'], $_POST['departure-date'], $_SESSION['customer_id'], $_POST['return-date'], $vehicle_data->vehicle_branch_id, $_POST['branch'], $vehicle_data->rate);
+  }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -66,7 +37,7 @@ $all_branches = $branch_obj->get_all_branches();
         <div class="background">
         <div class="booking-form">
             <h2>Rental Booking Form</h2>
-            <form action="index.php" method="post">  
+            <form action="" method="post">  
             <label for="branch">Branch</label>    
             <select name="branch" id="branch" required>
                 <option value="">--Select a Branch--</option>
