a8daac1cf76133b152ea79a4a1942e17d024b694
Brandon <itzbrandonlee@yahoo.com>
2024-12-12 13:41:00 -0800

    minor up updates, password req

--- booking_status.php
+++ booking_status.php
@@ -28,7 +28,7 @@ if(isset($_POST['status'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo  $vehicle_data->c_name;?></title>
+    <title><?php echo  $booking_data->confirmation_num;?></title>
     <link rel="stylesheet" href="./style.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
